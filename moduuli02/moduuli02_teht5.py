f67e60f8e2bf37f2d5c94189d9ae7e0ee6327f5c
aaroj <aaro.jylhamaki@outlook.com>
2024-01-16 17:25:16 +0200

    muutos

--- moduuli02/moduuli02_teht5.py
+++ moduuli02/moduuli02_teht5.py
@@ -7,4 +7,4 @@ print("")
 
 kokonaispaino = float(leiviskat)*20*32*13.3 + float(naulat)*32*13.3 + float(luodit)*13.3
 
-print(f"\nMassa nykymittojen mukaan:\n{int(kokonaispaino/1000)} kilogrammaa ja {kokonaispaino - int(kokonaispaino/1000)*1000:.2f} grammaa.")
+print(f"Massa nykymittojen mukaan:\n{int(kokonaispaino/1000)} kilogrammaa ja {kokonaispaino - int(kokonaispaino/1000)*1000:.2f} grammaa.")
