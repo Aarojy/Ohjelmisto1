280f38575774343871716e2cfe68d9277ce29a4e
aaroj <aaro.jylhamaki@outlook.com>
2024-01-30 16:16:41 +0200

    moduuli06:n lisäys

--- moduuli06/moduuli06_teht6.py
+++ moduuli06/moduuli06_teht6.py
@@ -12,12 +12,12 @@ if __name__ == '__main__':
     pizza2_diameter_cm = float(input("Syötä pizzan 2 halkaisija senttimetreinä: "))
     pizza2_price = float(input("Syötä pizzan 2 hinta euroina: "))
 
-    print(f"Pizzan 1 hinta on: {price_per_squaremeter(pizza1_diameter_cm,pizza1_price):.2f} euroa/neliömetri")
-    print(f"Pizzan 2 hinta on: {price_per_squaremeter(pizza2_diameter_cm, pizza2_price):.2f} euroa/neliömetri")
+    print(f"Pizzan 1 hinta on: {price_per_squaremeter(pizza1_diameter_cm,pizza1_price):.2f} euroa/neliömetri.")
+    print(f"Pizzan 2 hinta on: {price_per_squaremeter(pizza2_diameter_cm, pizza2_price):.2f} euroa/neliömetri.")
 
     if price_per_squaremeter(pizza1_diameter_cm,pizza1_price) == price_per_squaremeter(pizza2_diameter_cm, pizza2_price):
-        print("Pizzan 1 ja 2 antavat yhtä suuren vastineen rahalle")
+        print("Pizzan 1 ja 2 antavat yhtä suuren vastineen rahalle.")
     elif price_per_squaremeter(pizza1_diameter_cm,pizza1_price) < price_per_squaremeter(pizza2_diameter_cm, pizza2_price):
-        print("Pizzan 1 antaa paremman vastineen rahalle")
+        print("Pizzan 1 antaa paremman vastineen rahalle.")
     else:
-        print("Pizzan 2 antaa paremman vastineen rahalle")
+        print("Pizzan 2 antaa paremman vastineen rahalle.")
