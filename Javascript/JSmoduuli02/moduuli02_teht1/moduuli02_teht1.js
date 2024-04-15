523983fadb10fb4503b1d0ac0db2b6530c0d343c
aaroj <aaro.jylhamaki@outlook.com>
2024-04-15 21:06:52 +0300

    JS moduulin 2 teht 1:n korjaus

--- Javascript/JSmoduuli02/moduuli02_teht1/moduuli02_teht1.js
+++ Javascript/JSmoduuli02/moduuli02_teht1/moduuli02_teht1.js
@@ -4,7 +4,6 @@ let listOfNames = []
 
 for (let i = 1; i<=5; i++) {
   listOfNames.push(prompt(`Syötä ${i}. numero:`))
-  console.log(listOfNames)
 }
 
 for (let i = listOfNames.length-1; i>=0; i--) {
